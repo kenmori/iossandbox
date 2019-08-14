7649a1e041db16fc50a6bf92827644365221aa7d
kenjimorita <>
2019-08-15 07:28:10 +0900

    init swiftstart

---
+++ SwiftStart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftStart
+//
+//  Created by kenjimorita on 2019/08/15.
+//  Copyright © 2019年 kenjimorita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
